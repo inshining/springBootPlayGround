59081dfdaa60aea77f59791698926e48f03e1bee
inshining <inshining.kim@gmail.com>
2023-07-19 15:54:59 +0900

    ♻️[REFACTOR] : user name 정보 어노테이션으로 변경

--- src/main/java/com/playground/springboot/web/IndexController.java
+++ src/main/java/com/playground/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.playground.springboot.web;
 
+import com.playground.springboot.config.auth.LoginUser;
 import com.playground.springboot.config.auth.dto.SessionUser;
 import com.playground.springboot.service.posts.PostsService;
 import com.playground.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
